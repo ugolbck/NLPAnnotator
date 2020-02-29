eff34d2005fddc0ec8359297b195d9e2ba23f80c
Ugo Loobuyck <ugo813@hotmail.fr>
2020-02-29 21:46:01 +0100

    Harder than I thought

--- front.py
+++ front.py
@@ -8,7 +8,7 @@ class Application(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
         tk.Tk.title(self, "NLP Annotation tool")
-        tk.Tk.geometry(self, '750x300')
+        tk.Tk.geometry(self, '1400x500')
 
         window = tk.Frame(self)
         window.pack(side='top', fill='both', expand=True)
@@ -31,31 +31,31 @@ class Application(tk.Tk):
         frame.tkraise()
 
 
-def f():
-    return 'a'
-def f1():
-    print('Catégorie "b" choisie')
-
 class StartPage(tk.Frame):
     def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
+        tk.Frame.__init__(self, parent, relief='sunken', borderwidth=2)
 
         self.dataframe = DataF('1460_amazon_reviews.csv')
 
-        label = ttk.Label(self, text=self.dataframe.get_row(0), font=("Verdana", 12))
-        label.grid(column=1, row=1, sticky='n')
+        cat = tk.StringVar()
+        category = ttk.Entry(self, textvariable=cat)
+        category.grid(column=0, row=1, sticky=tk.W)
+        print(category.get())
 
-        button1 = ttk.Button(self, text='a', command=lambda: f())
+        label = ttk.Label(self, text=self.dataframe.get_row(0), font=("Verdana", 12))
+        label.grid(column=0, row=0, sticky=tk.N)
+
+        button1 = ttk.Button(self, text='a', width=70)
         button1.grid(column=0, row=3)
 
-        button2 = ttk.Button(self, text='b', command=f1)
-        button2.grid(column=1, row=3)
+        button2 = ttk.Button(self, text='b', width=70)
+        button2.grid(column=2, row=3)
 
-        button3 = ttk.Button(self, text='c', command=lambda: f())
+        button3 = ttk.Button(self, text='c', width=70, command="hello")
         button3.grid(column=0, row=6)
 
-        button4 = ttk.Button(self, text='d', command=lambda: f())
-        button4.grid(column=1, row=6)
+        button4 = ttk.Button(self, text='d', width=70)
+        button4.grid(column=2, row=6)
 
 
 app = Application()
